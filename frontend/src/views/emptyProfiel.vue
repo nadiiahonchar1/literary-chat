84936b38c2e9e520e6484989bb74c739339db2af
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-02-08 17:25:27 +0200

    added component ButtonWithIcon

---
+++ frontend/src/views/emptyProfiel.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="container">
+    <button-with-icon iconName="back-arrow.svg" @clicked="goBack" />
+    <h2>Мій профіль</h2>
+    <div class="container"></div>
+  </header>
+</template>
+
+<script>
+import ButtonWithIcon from "@/components/UI/ButtonWithIcon.vue";
+
+export default {
+  components: { ButtonWithIcon },
+  data() {
+    return {
+      backIcon: "@/assets/icons/back-arrow.svg",
+    };
+  },
+  methods: {
+    goBack() {
+      window.history.back();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
