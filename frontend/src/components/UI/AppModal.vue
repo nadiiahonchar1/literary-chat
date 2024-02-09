4de9f73dff7ec4722a8b46f75a0e48ded027c72a
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-02-09 18:17:34 +0200

    added your-info for emptyProfiel

---
+++ frontend/src/components/UI/AppModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="modal-backdrop" @click="$emit('close')">
+    <div class="modal">
+      <h3>This is the name of the modal</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quis
+        excepturi architecto tempore ipsam dolore fugiat voluptatibus aperiam!
+        Quidem, corporis.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["close"],
+};
+</script>
+
+<style scoped></style>
--- frontend/src/views/emptyProfiel.vue
+++ frontend/src/views/emptyProfiel.vue
@@ -4,18 +4,38 @@
       <button-with-icon iconName="back-arrow.svg" @clicked="goBack" />
       <h2>Мій профіль</h2>
     </header>
-    <div class="container"></div>
+    <div class="container smmall-top-padding">
+      <div class="yout-info">
+        <div class="your-photo">
+          <img
+            class="your-photo-icon"
+            src="@/assets/icons/book-open-reader.svg"
+            alt="Фото вашого профілю"
+          />
+        </div>
+        <p class="your-nickname" v-text="yourNickname"></p>
+        <button class="change-photo-button" @click="modal = true">
+          Змінити фото
+        </button>
+        <teleport to="#modal">
+          <app-modal v-if="modal" @close="modal = false"></app-modal>
+        </teleport>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ButtonWithIcon from "@/components/UI/ButtonWithIcon.vue";
+import AppModal from "@/components/UI/AppModal.vue";
 
 export default {
-  components: { ButtonWithIcon },
+  components: { ButtonWithIcon, AppModal },
   data() {
     return {
       backIcon: "@/assets/icons/back-arrow.svg",
+      yourNickname: "@yourNickname",
+      modal: false,
     };
   },
   methods: {
