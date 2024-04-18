b8d0cc22a883346cde5305ed17219c4ab7812e4a
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-04-18 19:13:33 +0300

    fix commented pinia in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,6 @@ import App from './App.vue'
 import router from './router';
 import './assets/main.scss'
 
-const pinia = createPinia();
+// const pinia = createPinia();
 // createApp(App).use(router).use(pinia).mount("#app");
 createApp(App).use(router).mount("#app");
