861a95f5e98dc1319c8f96c9b97f0c362fcd8796
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-04-18 17:34:04 +0300

    added icons sprite

--- frontend/src/views/mainPage.vue
+++ frontend/src/views/mainPage.vue
@@ -9,13 +9,29 @@
       <li v-for="chat in chats" :key="chat.id" class="chat-item">
         <router-link :to="`/${chat.iconName}`" class="chat-link">
           <div class="icon-border">
-            <img
+            <!-- <img
               :src="require(`@/assets/icons/${chat.iconName}.svg`)"
               :alt="chat.name"
               class="chat-icon"
-            />
+            /> -->
+            <svg class="chat-icon" width="40" height="40" viewBox="0 0 40 40">
+              <use
+                :xlink:href="
+                  require('@/assets/icons/chatIcons.svg') + '#' + chat.iconName
+                "
+              ></use>
+            </svg>
           </div>
           <span class="chat-name">{{ chat.name }}</span>
+          <p v-if="!chat.numberOfPersons">Тут поки що нічого немає.</p>
+          <div v-if="chat.numberOfPersons">
+            <img
+              :src="require(`@/assets/icons/person.svg`)"
+              :alt="chat.name"
+              class="chat-icon"
+            />
+            <span>{{ chat.numberOfPersons }}</span>
+          </div>
         </router-link>
       </li>
     </ul>
