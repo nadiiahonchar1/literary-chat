accb9863d3d80293460992dd977c02e7a1d6401a
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-04-18 19:01:19 +0300

    added style to person count

--- frontend/src/views/mainPage.vue
+++ frontend/src/views/mainPage.vue
@@ -5,34 +5,17 @@
       <h2>Кімнати</h2>
       <button-with-icon iconName="search.svg" />
     </header>
+    <div class="chat-item container-chat" :key="chatWithId1.id">
+      <chat-link :chat-data="chatWithId1" :attached="false" />
+    </div>
     <ul class="container-chat">
-      <li v-for="chat in chats" :key="chat.id" class="chat-item">
-        <router-link :to="`/${chat.iconName}`" class="chat-link">
-          <div class="icon-border">
-            <!-- <img
-              :src="require(`@/assets/icons/${chat.iconName}.svg`)"
-              :alt="chat.name"
-              class="chat-icon"
-            /> -->
-            <svg class="chat-icon" width="40" height="40" viewBox="0 0 40 40">
-              <use
-                :xlink:href="
-                  require('@/assets/icons/chatIcons.svg') + '#' + chat.iconName
-                "
-              ></use>
-            </svg>
-          </div>
-          <span class="chat-name">{{ chat.name }}</span>
-          <p v-if="!chat.numberOfPersons">Тут поки що нічого немає.</p>
-          <div v-if="chat.numberOfPersons">
-            <img
-              :src="require(`@/assets/icons/person.svg`)"
-              :alt="chat.name"
-              class="chat-icon"
-            />
-            <span>{{ chat.numberOfPersons }}</span>
-          </div>
-        </router-link>
+      <li v-for="chat in attachedChats" :key="chat.id" class="chat-item">
+        <chat-link :chat-data="chat" :attached="true" />
+      </li>
+    </ul>
+    <ul class="container-chat">
+      <li v-for="chat in unattachedChats" :key="chat.id" class="chat-item">
+        <chat-link :chat-data="chat" :attached="false" />
       </li>
     </ul>
   </div>
@@ -41,15 +24,30 @@
 <script>
 import chatsList from "@/chat-list.json";
 import ButtonWithIcon from "@/components/UI/ButtonWithIcon.vue";
+import ChatLink from "@/components/UI/ChatLink.vue";
 
 export default {
   components: {
     ButtonWithIcon,
+    ChatLink,
   },
   data() {
     return {
       chats: chatsList,
     };
+  },
+  computed: {
+    attachedChats() {
+      return this.chats.filter((chat) => chat.attached === true);
+    },
+    unattachedChats() {
+      return this.chats.filter(
+        (chat) => chat.attached === false && chat.id !== 1
+      );
+    },
+    chatWithId1() {
+      return this.chats.find((chat) => chat.id === 1);
+    },
   },
 };
 </script>
