ed2d1b6fb840b350117516cc6eb82a1e2f9331ed
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-04-18 19:10:36 +0300

    comented pinia in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
-import { createPinia } from "pinia";
+// import { createPinia } from "pinia";
 import App from './App.vue'
 import router from './router';
 import './assets/main.scss'
 
 const pinia = createPinia();
-createApp(App).use(router).use(pinia).mount("#app");
+// createApp(App).use(router).use(pinia).mount("#app");
+createApp(App).use(router).mount("#app");
