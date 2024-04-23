2a280e1c451577182e023d9ef858a3dbd7198378
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-04-23 18:17:10 +0300

    fix Nickname verification

--- frontend/src/views/greetings/getAcquaintance.vue
+++ frontend/src/views/greetings/getAcquaintance.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <Transition name="fade" appear>
     <div class="slide container" v-cloak>
       <div class="content-container">
@@ -21,8 +21,8 @@
           <form>
             <app-input label="Нікнейм" v-model.trim="nikname"></app-input>
             <p class="acquaintance__explanation">
-              Нікнейм може містити лише латинські літери і цифри.
-              Лише латинські літери і цифри.
+              Нікнейм може містити лише латинські літери і цифри. Лише латинські
+              літери і цифри.
             </p>
             <div class="btn-container">
               <btn
@@ -66,7 +66,7 @@ export default {
     btn,
     preload,
   },
-   computed: {
+  computed: {
     isNextDisabled() {
       if (!this.nikname) {
         this.skipError();
@@ -79,9 +79,8 @@ export default {
       }
       this.skipError();
       return false;
-      // return !this.nikname;
     },
-  },  
+  },
   watch: {
     IsActive(value) {
       if (value) {
@@ -93,7 +92,7 @@ export default {
       }
     },
   },
-  
+
   methods: {
     skipall() {
       this.nikname = "";
@@ -106,14 +105,6 @@ export default {
     },
     setErrorMessage() {
       this.nicknameError = "Лише латинські літери і цифри.";
-    },
-    submitHandler() {
-      if (this.niknameValidate()) {
-        localStorage.setItem("nikname", `@${this.nikname}`);
-        this.$router.push("profiel");
-      } else {
-        this.setErrorMessage();
-      }
     },
     submitAlldata() {
       this.isLoaded = true;
@@ -137,7 +128,166 @@ export default {
         .then((response) => {
           console.log(response.data);
           this.isLoaded = false;
-          localStorage.setItem('nikname', `${this.nikname}`)
+          localStorage.setItem("nikname", `${this.nikname}`);
+          this.$router.push("registcode");
+        })
+        .catch((error) => {
+          console.error(error);
+          this.isLoaded = false;
+        });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style> -->
+
+<template>
+  <Transition name="fade" appear>
+    <div class="slide container" v-cloak>
+      <div class="content-container">
+        <div class="background-onbording background-onbording-sixth">
+          <img
+            class="get-acquainted"
+            src="@/assets/img/get-acquainted.svg"
+            alt="Застосунок готовий познайомитись з вами"
+          />
+        </div>
+        <div class="slide__text acquaintance__text">
+          <h2 class="acquaintance__tittle">Познайомимося?</h2>
+          <h3 class="acquaintance__description">
+            Придумай унікальний нікнейм, <br />
+            за яким тебе можуть знайти однодумці.
+          </h3>
+          <p class="acquaintance__caption">
+            Додаткову інформацію ти завжди зможеш внести в особистому кабінеті.
+          </p>
+          <form @submit.prevent="submitAlldata">
+            <app-input
+              label="Нікнейм"
+              v-model.trim="nikname"
+              :error="nicknameError"
+            ></app-input>
+            <p
+              class="acquaintance__explanation"
+              :class="{ 'text-hidden': nicknameError }"
+            >
+              Лише латинські літери і цифри.
+            </p>
+            <div class="btn-container">
+              <btn
+                :title="buttonTitle"
+                @skip-all="skipall"
+                :skiptext="buttonskip"
+                firstButtonType="submit"
+                :first-button-disabled="isNextDisabled"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="prel" v-if="isLoaded">
+        <div class="slide__prell">
+          <preload></preload>
+        </div>
+      </div>
+      <div class="slide__prell" v-else></div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+import axios from "axios";
+import AppInput from "@/components/UI/AppInput.vue";
+import btn from "@/components/greetings/button-table.vue";
+import preload from "@/components/greetings/preload/pre-load.vue";
+
+export default {
+  data() {
+    return {
+      nikname: "",
+      buttonTitle: "Зберегти та увійти",
+      buttonskip: "",
+      nicknameError: false,
+      isLoaded: false,
+      IsActive: false,
+    };
+  },
+  components: {
+    AppInput,
+    btn,
+    preload,
+  },
+  computed: {
+    isNextDisabled() {
+      if (!this.nikname) {
+        this.skipError();
+        return true;
+      } else {
+        if (!/^[a-zA-Z0-9]+$/.test(this.nikname)) {
+          this.setErrorMessage();
+          return true;
+        }
+      }
+      this.skipError();
+      return false;
+    },
+  },
+  watch: {
+    IsActive(value) {
+      if (value) {
+        document.querySelector(".slide").classList.add("active");
+        document.querySelector(".content-container").classList.add("load");
+      } else {
+        document.querySelector(".slide").classList.remove("active");
+        document.querySelector(".content-container").classList.remove("load");
+      }
+    },
+  },
+  methods: {
+    skipall() {
+      this.nikname = "";
+    },
+    skipError() {
+      this.nicknameError = "";
+    },
+    niknameValidate() {
+      return /^[a-zA-Z0-9]+$/.test(this.nikname);
+    },
+    setErrorMessage() {
+      this.nicknameError = "Лише латинські літери і цифри.";
+    },
+    // submitHandler() {
+    //   if (this.niknameValidate()) {
+    //     localStorage.setItem("nikname", `@${this.nikname}`);
+    //     this.$router.push("profiel");
+    //   } else {
+    //     this.setErrorMessage();
+    //   }
+    // },
+    submitAlldata() {
+      this.isLoaded = true;
+      this.IsActive = !this.IsActive;
+      const headers = {
+        "api-key": "6A38900C07D34C4F9839226B66FBEA24",
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      };
+      const body = {
+        username: this.nikname,
+        email: localStorage.getItem("newemail"),
+      };
+      console.log(body);
+      axios
+        .post(
+          "https://klabapi.azurewebsites.net/api/authentication/sign-up",
+          body,
+          { headers }
+        )
+        .then((response) => {
+          console.log(response.data);
+          this.isLoaded = false;
+          localStorage.setItem("nikname", `${this.nikname}`);
           this.$router.push("registcode");
         })
         .catch((error) => {
--- frontend/src/views/greetings/select-code.vue
+++ frontend/src/views/greetings/select-code.vue
@@ -3,63 +3,64 @@
     <div class="slide container">
       <div class="content-container">
         <div class="background-onbording background-onbording-seven">
-            <img
-                class="boy-with-laptop"
-                src="@/assets/img/registform.svg"
-                alt="Застосунок готовий познайомитись з вами"
-            />
+          <img
+            class="boy-with-laptop"
+            src="@/assets/img/registform.svg"
+            alt="Застосунок готовий познайомитись з вами"
+          />
         </div>
         <div class="slide__text">
           <h2 class="regist__title regsit-style">Підтвердження</h2>
           <h3 class="regist__description regsit-descript">
-            Введи 4-значний код,<br> який ми відправили на вказану адресу 
+            Введи 4-значний код,<br />
+            який ми відправили на вказану адресу
           </h3>
         </div>
-        <div class="slide__code"  >
-            <input 
-           
-                v-model="valueCode[0]"
-                type="tel"
-                maxlength = "1"
-                class="slide__item"
-                @input="tonext(0)"
-              >
-              <input 
-                v-model="valueCode[1]"
-                type="tel"
-                maxlength = "1"
-                class="slide__item"
-                @input="tonext(1)"
-                ref="input1"
-              >
-              <input
-                v-model="valueCode[2]"
-                type="tel"
-                maxlength = "1"
-                class="slide__item"
-                @input="tonext(2)"
-                ref="input2"
-              >
-              <input 
-                v-model="valueCode[3]"
-                type="tel"
-                maxlength = "1"
-                class="slide__item"
-                @input="tonext(3)"
-                ref="input3"
-              >
+        <div class="slide__code">
+          <input
+            v-model="valueCode[0]"
+            type="tel"
+            maxlength="1"
+            class="slide__item"
+            @input="tonext(0)"
+          />
+          <input
+            v-model="valueCode[1]"
+            type="tel"
+            maxlength="1"
+            class="slide__item"
+            @input="tonext(1)"
+            ref="input1"
+          />
+          <input
+            v-model="valueCode[2]"
+            type="tel"
+            maxlength="1"
+            class="slide__item"
+            @input="tonext(2)"
+            ref="input2"
+          />
+          <input
+            v-model="valueCode[3]"
+            type="tel"
+            maxlength="1"
+            class="slide__item"
+            @input="tonext(3)"
+            ref="input3"
+          />
         </div>
         <div v-if="erorr">
           {{ erorrMesage }}
         </div>
         <div class="slide__descript">
-           <div class="regist__description regsit-descript">Код підтвердження не отримано?</div>
-           <div class="regist__description regsit-descript">Відправити ще раз</div>
+          <div class="regist__description regsit-descript">
+            Код підтвердження не отримано?
+          </div>
+          <div class="regist__description regsit-descript">
+            Відправити ще раз
+          </div>
         </div>
-        <btn 
-        :title="buttonTitle"
-        @click="save"
-        />
+        <btn :title="buttonTitle" @click="save" />
       </div>
       <div class="prel" v-if="isLoaded">
         <div class="slide__prell">
@@ -68,111 +69,114 @@
       </div>
       <div class="slide__prell" v-else></div>
     </div>
-</Transition>        
+  </Transition>
 </template>
-
 
 <script>
 import btn from "@/components/greetings/button-table.vue";
 import preload from "@/components/greetings/preload/pre-load.vue";
 import axios from "axios";
 
-
 export default {
-  data(){
-    return{
-      buttonTitle : 'Увійти',
-      valueCode: ['','','',''],
+  data() {
+    return {
+      buttonTitle: "Увійти",
+      valueCode: ["", "", "", ""],
       isLoaded: false,
       IsActive: false,
       erorr: false,
-      erorrMesage: 'Код введений невірно. Будь ласка, спробуй ще раз.'
-    }
+      erorrMesage: "Код введений невірно. Будь ласка, спробуй ще раз.",
+    };
   },
-  components:{
+  components: {
     btn,
-    preload
+    preload,
   },
   methods: {
-    getkeyLocalstore(keys){
-    for(let key of keys){
-      let value = localStorage.getItem(key)
-      if(value !== null){
-        return value;
+    getkeyLocalstore(keys) {
+      for (let key of keys) {
+        let value = localStorage.getItem(key);
+        if (value !== null) {
+          return value;
+        }
       }
-    }
-    return null
-  },
-  tonext(index) {
-    if (this.valueCode[index].length >= 1) {
-        const inputs = [this.$refs.input0, this.$refs.input1, this.$refs.input2, this.$refs.input3];
+      return null;
+    },
+    tonext(index) {
+      if (this.valueCode[index].length >= 1) {
+        const inputs = [
+          this.$refs.input0,
+          this.$refs.input1,
+          this.$refs.input2,
+          this.$refs.input3,
+        ];
         if (index < inputs.length - 1) {
           inputs[index + 1].focus();
         }
-    }
-  },
+      }
+    },
     save() {
-    this.isLoaded = true;
-    this.IsActive = !this.IsActive;
-    const email = this.getkeyLocalstore(['newemail'])
-    const сurrentemail = this.getkeyLocalstore(['сurrentemail'])
-    const verificationCode = this.valueCode.join('');
-  const headers = {
-    "api-key": "6A38900C07D34C4F9839226B66FBEA24",
-    "Content-Type": "application/json",
-    "X-Requested-With": "XMLHttpRequest",
-  };
+      this.isLoaded = true;
+      this.IsActive = !this.IsActive;
+      const email = this.getkeyLocalstore(["newemail"]);
+      const сurrentemail = this.getkeyLocalstore(["сurrentemail"]);
+      const verificationCode = this.valueCode.join("");
+      const headers = {
+        "api-key": "6A38900C07D34C4F9839226B66FBEA24",
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+      };
 
-  if (сurrentemail == null) {
-    const data = {
-      email: email,
-      verificationCode: verificationCode,
-    };
-    axios
-      .post(
-        "https://klabapi.azurewebsites.net/api/authentication/verify-email",
-        data,
-        { headers }
-      )
-      .then((response) => {
-        console.log(response.data);
-        this.$router.push('profiel')
-        this.isLoaded = false;
-      })
-      .catch(() => {
-        // console.error(error);
-        // console.error(error.response.data)
-        this.erorr = true
-        this.isLoaded = false
-        this.IsActive = false
-        this.showError = true;
-      });
-  } else {
-    const data = {
-      email: сurrentemail,
-      authenticationCode: verificationCode,
-    };
-    axios
-      .post(
-        "https://klabapi.azurewebsites.net/api/authentication/authenticate-email",
-        data,
-        { headers }
-      )
-      .then((response) => {
-        this.$router.push('profiel')
-        console.log(response.data);
-        this.isLoaded = false;
-      })
-      .catch(() => {
-        this.erorr = true
-        this.isLoaded = false
-        this.IsActive = false
-        this.showError = true;
-      });
-  }
-}
-},
-watch: {
+      if (сurrentemail == null) {
+        const data = {
+          email: email,
+          verificationCode: verificationCode,
+        };
+        axios
+          .post(
+            "https://klabapi.azurewebsites.net/api/authentication/verify-email",
+            data,
+            { headers }
+          )
+          .then((response) => {
+            console.log(response.data);
+            this.$router.push("profiel");
+            this.isLoaded = false;
+          })
+          .catch(() => {
+            // console.error(error);
+            // console.error(error.response.data)
+            this.erorr = true;
+            this.isLoaded = false;
+            this.IsActive = false;
+            this.showError = true;
+          });
+      } else {
+        const data = {
+          email: сurrentemail,
+          authenticationCode: verificationCode,
+        };
+        axios
+          .post(
+            "https://klabapi.azurewebsites.net/api/authentication/authenticate-email",
+            data,
+            { headers }
+          )
+          .then((response) => {
+            this.$router.push("profiel");
+            console.log(response.data);
+            this.isLoaded = false;
+          })
+          .catch(() => {
+            this.erorr = true;
+            this.isLoaded = false;
+            this.IsActive = false;
+            this.showError = true;
+          });
+      }
+    },
+  },
+  watch: {
     IsActive(value) {
       if (value) {
         document.querySelector(".slide").classList.add("active");
@@ -183,10 +187,10 @@ watch: {
       }
     },
   },
-mounted() {
-  this.$refs.inputs = this.$el.querySelectorAll('input');
-},
-}
+  mounted() {
+    this.$refs.inputs = this.$el.querySelectorAll("input");
+  },
+};
 </script>
 
 <style>
